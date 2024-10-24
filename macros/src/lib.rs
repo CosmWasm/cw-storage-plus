dee47e4261308cf92ffa3b5aff855f6ed7cb6b9a
Tomasz Kurcz <uint@users.noreply.github.com>
2024-10-24 20:20:21 +0200

    prevent inconsistent PK in Index implementors

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -7,9 +7,9 @@ For more information on this package, please check out the
 
 use proc_macro::TokenStream;
 use syn::{
-    Ident,
+    Ident, Type, TypePath,
     __private::{quote::quote, Span},
-    parse_macro_input, ItemStruct,
+    parse_macro_input, ItemStruct, Lifetime,
 };
 
 #[proc_macro_attribute]
@@ -29,12 +29,28 @@ pub fn index_list(attr: TokenStream, item: TokenStream) -> TokenStream {
         })
         .collect::<Vec<_>>();
 
+    let mut first_ix_type = input.fields.iter().next().unwrap().ty.clone();
+    // change all lifetime params to 'static
+    if let Type::Path(TypePath { ref mut path, .. }) = first_ix_type {
+        path.segments.iter_mut().for_each(|seg| {
+            if let syn::PathArguments::AngleBracketed(ref mut args) = seg.arguments {
+                for arg in args.args.iter_mut() {
+                    if let syn::GenericArgument::Lifetime(ref mut lt) = arg {
+                        *lt = Lifetime::new("'static", Span::call_site());
+                    }
+                }
+            }
+        });
+    }
+
     let expanded = quote! {
         #input
 
         impl cw_storage_plus::IndexList<#ty> for #struct_ty<'_> {
-            fn get_indexes(&'_ self) -> Box<dyn Iterator<Item = &'_ dyn cw_storage_plus::Index<#ty>> + '_> {
-                let v: Vec<&dyn cw_storage_plus::Index<#ty>> = vec![#(#names),*];
+            type PK = <#first_ix_type as ::cw_storage_plus::Index<#ty>>::PK;
+
+            fn get_indexes(&'_ self) -> Box<dyn Iterator<Item = &'_ dyn cw_storage_plus::Index<#ty, PK = Self::PK>> + '_> {
+                let v: Vec<&dyn cw_storage_plus::Index<#ty, PK = Self::PK>> = vec![#(#names),*];
                 Box::new(v.into_iter())
             }
         }
